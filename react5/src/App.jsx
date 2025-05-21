d7f0cc5c979c21125957224ceb87b0e8c1f9bfa6
akmanil <maharanaanil424@gmail.com>
2025-05-21 13:29:52 +0530

    event handler learn in  react

--- react5/src/App.jsx
+++ react5/src/App.jsx
@@ -4,30 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count ,setCount] = useState(0);
+  
+  const Event = () =>{
+   setCount(count+1);
 
+  }
+  const Enter = () =>{
+    alert("its btn 2")
+  }  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className ="flex flex-col">
+      <button onClick={() =>Event()}>Click {count}</button>
+      <button onMouseEnter={() =>Enter()}>Click</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
