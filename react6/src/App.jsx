ed748885302f174bb9a0e57e03d7c939c6b62f5e
akmanil <maharanaanil424@gmail.com>
2025-05-22 11:50:08 +0530

    Changing the toggle app for switching the light and dark mode

--- react6/src/App.jsx
+++ react6/src/App.jsx
@@ -1,25 +1,30 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [On, setOn] = useState(true)
+  const [On, setOn] = useState(true);
 
-  const toggleSwitch =()=>{
+  const toggleSwitch = () => {
     setOn(!On);
-  }
+  };
 
   return (
     <>
-      <div className="par" onClick= {toggleSwitch} 
-      style={{backgroundColor : On ? "green" :"Red"}}>
-        <div className={`chi ${On ? "on" : "off"}`}>
-          <span>{On ? "ON" : "OFF"}</span>
+      <div style={{ backgroundColor: On ? "Black" : "White" }} className="body">
+        <div
+          className="par"
+          onClick={toggleSwitch}
+          style={{ backgroundColor: On ? "green" : "Red" }}
+        >
+          <div className={`chi ${On ? "on" : "off"}`}>
+            <span>{On ? "Dark" : "Light"}</span>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
